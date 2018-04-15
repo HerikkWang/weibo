e06c4d3bc64efe4b64b6a06da9480df755a02de9
HerikkWang <m18158515131@163.com>
2018-04-15 23:47:09 +0800

    将数据存入mysql

--- scrapy_1.py
+++ scrapy_1.py
@@ -4,25 +4,30 @@ import html5lib
 import Login
 from urllib.request import urlopen
 import csv
+import pymysql
 
 # scrape the data, whose attributes include question_content, question_value, questioner_name,
 # questioner_id, replier_name, replier_id, question_time
 
-csv_write_path = r"C:\Users\Administrator\PycharmProjects\weibo\weibo_data_2_1.csv"
-csv_read_path = 'weibo_data_4_processed.csv'
+
+csv_read_path = 'weibo_new_page_url.csv'
+
+conn = pymysql.connect(host='localhost', user='root', password='herik', database='weibodata', charset='utf8mb4')
+cur = conn.cursor()
+cur.execute('use weibodata')
 
 
 def get_data_1(url):
     try:
         html = urlopen(url)
-        bsObj = BeautifulSoup(html.read(),'lxml')
-        question_content = bsObj.find('div',{'class':'ask_con'}).get_text().strip()
-        question_value = bsObj.find('em',{'class':'S_spetxt'}).get_text().strip()[1:]
-        questioner_name = bsObj.find_all('a',{'class':'S_txt1'})[1].get_text().strip()
-        questioner_id = bsObj.find_all('a',{'class':'S_txt1'})[1].attrs['href'][3:]
-        replier_name = bsObj.find_all('a',{'class':'S_txt1'})[3].get_text().strip()
-        replier_id = bsObj.find_all('a',{'class':'S_txt1'})[3].attrs['href'][3:]
-        question_time = bsObj.find('div',{'class':'S_txt2'}).get_text()[:-2]
+        bsObj = BeautifulSoup(html.read(), 'lxml')
+        question_content = bsObj.find('div', {'class': 'ask_con'}).get_text().strip()
+        question_value = bsObj.find('em', {'class': 'S_spetxt'}).get_text().strip()[1:]
+        questioner_name = bsObj.find_all('a', {'class': 'S_txt1'})[1].get_text().strip()
+        questioner_id = bsObj.find_all('a', {'class': 'S_txt1'})[1].attrs['href'][3:]
+        replier_name = bsObj.find_all('a', {'class': 'S_txt1'})[3].get_text().strip()
+        replier_id = bsObj.find_all('a', {'class': 'S_txt1'})[3].attrs['href'][3:]
+        question_time = bsObj.find('div', {'class': 'S_txt2'}).get_text()[:-2]
     except:
         question_content = ''
         question_value = ''
@@ -32,21 +37,21 @@ def get_data_1(url):
         replier_name = ''
         question_time = ''
     data = [question_content, question_value, questioner_name, questioner_id, replier_name, replier_id, question_time]
+    print(question_content + '\n' + question_value + '\n' + questioner_name + '\n' + questioner_id + '\n'
+          + replier_name + '\n' + replier_id + '\n' + question_time)
     return data
-    # print(question_content+'\n'+question_value+'\n'+questioner_name+'\n'+questioner_id+'\n'
-          # +replier_name+'\n'+replier_id+'\n'+question_time)
+
 
 Login.login()
-datas = [['question_content', 'question_value', 'questioner_name', 'questioner_id', 'replier_name', 'replier_id', 'question_time', 'page_url']]
-with open(csv_read_path) as f:
+with open(csv_read_path, 'r', encoding='utf-8') as f:
     reader = csv.DictReader(f)
     for row in reader:
         page_url = row['page_url']
         data = get_data_1(page_url)
-        data.append(page_url)
-        datas.append(data)
+        cur.execute('''insert into weibo_scrapy_1 (page_url, question_content, question_value, questioner_name,
+        questioner_id, replier_name, replier_id, question_time) values (\"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\", \"%s\")
+        ''', (page_url, data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
+        cur.connection.commit()
 
-with open(csv_write_path, 'w+', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    for row in datas:
-        writer.writerow(row)
+cur.close()
+conn.close()
